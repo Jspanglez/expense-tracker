01ffd42efbdd13ef967486751d434d1924b7593b
Jeev Pangli <jspanglez@gmail.com>
2024-04-25 18:10:24 +0100

    Imported Font Awesome

--- src/AddBtn.tsx
+++ src/AddBtn.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function AddBtn() {
   return (
-    <button>AddBtn</button>
-  )
+    <button type="button" className="btnAdd">
+      <FontAwesomeIcon icon={faPlus} />
+    </button>
+  );
 }
 
-export default AddBtn
+export default AddBtn;
