39ac65983bd39889c5db88aa3d218d43e04093a4
Jeev Pangli <jspanglez@gmail.com>
2024-04-25 18:11:03 +0100

    Moved the heading and the button into a container

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import AddBtn from "./AddBtn";
 
 const Sidebar = () => {
   return (
     <div className="sidebar-container">
-      <p className="sidebar-heading">Your Expenses</p>
+      <span className="sidebar-header">
+        <p className="sidebar-heading">Your Expenses</p>
+        <AddBtn />
+      </span>
       <div className="sidebar-list">
         <li>Item</li>
       </div>
