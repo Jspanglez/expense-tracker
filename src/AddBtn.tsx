43e198c66a4ceeb8beaa887fd3c2ad1690d7fafc
Jeev Pangli <jspanglez@gmail.com>
2024-06-23 11:03:21 +0100

    Added an interface

--- src/AddBtn.tsx
+++ src/AddBtn.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-function AddBtn() {
+interface Props {
+  onClick: () => void
+}
+
+function AddBtn({ onClick } : Props) {
   return (
-    <button type="button" className="btnAdd">
+    <button type="button" className="btnAdd" onClick={onClick}>
       <FontAwesomeIcon icon={faPlus} />
     </button>
   );
