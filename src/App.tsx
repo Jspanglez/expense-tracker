a617a9e12b2cf3e75c8c010b7257eceb8a0076e6
Jeev Pangli <jspanglez@gmail.com>
2024-06-23 11:01:50 +0100

    Created a function that adds elements to the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import AddBtn from "./AddBtn";
+import { useState } from "react";
+import ExpenseDetails from "./ExpenseDetails";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const [elements, setElements] = useState<{ name: string; details: string }[]>(
+    []
+  );
+
+  // Function to add a new element to the list
+  const addElement = () => {
+    const newElement = { name: "Name", details: "Description" };
+    setElements((prevElements) => [...prevElements, newElement]);
+  };
+
+  const myFunction = () => {
+    console.log("Click!");
+  };
+
   return (
     <div className="main">
-      <Sidebar />
+      <Sidebar
+        elements={elements}
+        addElement={addElement}
+        myFunction={myFunction}
+      />
+      <ExpenseDetails elements={elements}/>
     </div>
   );
 }
