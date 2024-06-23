562ebb0e9d187be9ecc77f70c420a86a0f4d8a4d
Jeev Pangli <jspanglez@gmail.com>
2024-06-23 11:05:41 +0100

    Added more elements to the Sidebar

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,21 +1,29 @@
-import React from "react";
 import "./App.css";
 import AddBtn from "./AddBtn";
+import ListElement from "./ListElement";
+
 interface Props {
   elements: { name: string; details: string }[];
   addElement: () => void;
   myFunction: () => void;
 }
 
-const Sidebar = () => {
+const Sidebar = ({ elements, addElement, myFunction }: Props) => {
   return (
     <div className="sidebar-container">
-      <span className="sidebar-header">
-        <p className="sidebar-heading">Your Expenses</p>
-        <AddBtn />
-      </span>
-      <div className="sidebar-list">
-        <li>Item</li>
+      <div className="sidebar-header">
+        <p className="sidebar-heading">My Expenses</p>
+        <AddBtn onClick={addElement} />
+      </div>
+      <div className="list-container">
+        {elements.map((element, index) => (
+          <ListElement
+            key={index}
+            name={element.name}
+            details={element.details}
+            onClick={myFunction}
+          />
+        ))}
       </div>
     </div>
   );
